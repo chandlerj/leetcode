1fd6d99841a582d23081b28c7501836f1640c0c1
Chandler Justice <chandler@cock.li>
2023-11-18 18:50:27 -0700

    solved max profit II

--- maxProfit.cpp
+++ maxProfit.cpp
@@ -12,12 +12,23 @@ int maxProfit(std::vector<int> &prices){
 		if(price - lowItem > mostProfit){
 			mostProfit = price - lowItem;
 		}
-		
 	}
 	return mostProfit;
 }
 
+int maxProfit2(std::vector<int> &prices){
+	int totalProfit = 0;
+	for(int i = 1; i < prices.size(); i++){ //for every day
+		if(prices[i] - prices[i-1] > 0){
+			totalProfit += prices[i] - prices[i-1];
+		}
+	}
+	return totalProfit;
+}
+
 int main(){
-	std::vector<int> prices = {7,6,4,3,1};
-	std::cout << maxProfit(prices) << std::endl;
+	std::vector<int> prices = {7,1,5,3,6,4};
+	int maxProfitMultipleDays = maxProfit2(prices);
+	std::cout << "Maxprofit with one share: " << maxProfit(prices) << std::endl;
+	std::cout << "maxprofit with multiple shares: " <<  maxProfitMultipleDays << std::endl;
 }
